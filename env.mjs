a9a79bd1c3252a507f2ea9dc77b6162faea06f0b
Harold Martin <harold.martin@gmail.com>
2025-11-08 17:17:12 -0700

    require site url and log redirect url

--- env.mjs
+++ env.mjs
@@ -1,21 +1,19 @@
 import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 
-const optionalNormalizedUrl = z
-  .preprocess((value) => {
-    if (typeof value !== "string" || value.length === 0) {
-      return undefined
-    }
+const normalizedUrl = z.preprocess((value) => {
+  if (typeof value !== "string" || value.length === 0) {
+    return undefined
+  }
 
-    return value.startsWith("http://") || value.startsWith("https://") ? value : `https://${value}`
-  }, z.string().url())
-  .optional()
+  return value.startsWith("http://") || value.startsWith("https://") ? value : `https://${value}`
+}, z.string().url())
 
 export const env = createEnv({
   server: {
     NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
-    NEXT_PUBLIC_VERCEL_URL: optionalNormalizedUrl,
+    NEXT_PUBLIC_VERCEL_URL: normalizedUrl,
     ANALYZE: z
       .enum(["true", "false"])
       .optional()
@@ -24,7 +22,7 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
-    NEXT_PUBLIC_VERCEL_URL: optionalNormalizedUrl,
+    NEXT_PUBLIC_VERCEL_URL: normalizedUrl,
   },
   runtimeEnv: {
     ANALYZE: process.env.ANALYZE,
